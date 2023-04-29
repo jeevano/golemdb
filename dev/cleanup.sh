4a1635ea0659e4e3ee012c16d9fe9c4a281e6d04
jeevano <>
2023-04-28 22:57:59 -0400

    add cleanup script

---
+++ dev/cleanup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+kill `lsof -t -i:8080`
+kill `lsof -t -i:8081`
+kill `lsof -t -i:8082`
--- dev/startup.sh
+++ dev/startup.sh
@@ -15,9 +15,3 @@ mkdir data/n3/
 sleep 5
 ./bin/golemdb -id=2 -kv-addr=localhost:8081 -raft-addr=localhost:4041 -join-addr=localhost:8080 -data-dir="data/n2/" -bootstrap=false &
 ./bin/golemdb -id=3 -kv-addr=localhost:8082 -raft-addr=localhost:4042 -join-addr=localhost:8080 -data-dir="data/n3/" -bootstrap=false &
-
-# How to end the processes:
-# run `ps` from same shell to get active process or
-# `lsof -i tcp:8080`
-
-# and `kill -9 <PID>`
